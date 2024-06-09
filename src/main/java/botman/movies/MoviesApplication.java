6d91e52d47f4af4890fba25048bba9f5a22bbd04
Manpreet4491 <129290161+Manpreet4491@users.noreply.github.com>
2024-06-09 16:33:49 +0530

    added-new-domain-cors

--- src/main/java/botman/movies/MoviesApplication.java
+++ src/main/java/botman/movies/MoviesApplication.java
@@ -18,7 +18,7 @@ public class MoviesApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://review-matter.vercel.app", "https://review-matter-76cth5u6x-manpreet4491s-projects.vercel.app", "https://review-matter-git-main-manpreet4491s-projects.vercel.app")
+                        .allowedOrigins("http://localhost:3000", "https://review-matter.vercel.app", "review-matter-manpreet4491-manpreet4491s-projects.vercel.app", "review-matter-gfl8o0mch-manpreet4491s-projects.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
